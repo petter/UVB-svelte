ba4eb993ff07bc99ff726a5f113e9e203b2d603b
Petter Sæther Moen <pettersmoen@gmail.com>
2019-12-24 17:46:10 +0100

    Added an expanding textfield to contact page. Still needs scroll fix

--- src/components/UI/Input.svelte
+++ src/components/UI/Input.svelte
@@ -1,4 +1,6 @@
 <script>
+    import { onMount } from 'svelte';
+
     let className = '';
     export { className as class };
     export let type = 'text';
@@ -7,18 +9,56 @@
     export let placeholder = '';
     export let id;
     export let value;
+
+    const classes = `p-2 text-gray-900 placeholder-gray-600 bg-gray-100 text-lg
+        rounded border-2 border-gray-600 focus:bg-white focus:border-gray-900
+        outline-none ${className}`;
+
+    let minHeight = '0px';
+    let height = 'auto';
+    let textareaRef;
+
+    onMount(() => {
+        if (type === 'textarea') {
+            minHeight = `${textareaRef.scrollHeight}px`;
+            height = minHeight;
+        }
+    });
+
+    const handleInput = event => {
+        if (type !== 'textarea') {
+            value = event.target.value;
+        } else {
+            event.target.height = '0px';
+            height = '0px';
+            requestAnimationFrame(
+                () => (height = `${event.target.scrollHeight}px`)
+            );
+        }
+    };
 </script>
 
 <div class="flex flex-col">
     <label for={id} class="text-sm text-indigo-900">{label}</label>
-    <input
-        {id}
-        {type}
-        {name}
-        {placeholder}
-        {value}
-        on:input={event => (value = event.target.value)}
-        class="p-2 text-gray-900 placeholder-gray-600 bg-gray-100 text-lg
-        rounded border-2 border-gray-600 focus:bg-white focus:border-gray-900
-        outline-none {className}" />
+    {#if type === 'textarea'}
+        <textarea
+            bind:this={textareaRef}
+            class="{classes} overflow-hidden resize-none"
+            rows="5"
+            {id}
+            {name}
+            {placeholder}
+            on:input|preventDefault={handleInput}
+            style="min-height: {minHeight}; height: {height}"
+            bind:value />
+    {:else}
+        <input
+            class={classes}
+            {id}
+            {type}
+            {name}
+            {placeholder}
+            {value}
+            on:input={event => (value = event.target.value)} />
+    {/if}
 </div>
--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -3,26 +3,34 @@
     import H1 from '../components/UI/H1.svelte';
 
     let name = '',
-        email = 'pettersmoen@gmail.com',
+        email = '',
         message = '';
 </script>
 
 <div class="flex flex-col max-w-4xl w-full mx-auto p-6">
 
-    <H1>Kontakt oss</H1>
+    <H1>For spørsmål eller timebestilling - kontakt oss her</H1>
     <form>
         <Input
-            label="Navn"
-            placeholder="Navn"
+            label="Ditt navn"
+            placeholder="Ola Nordmann"
             type="text"
             name="name"
             id="name"
             bind:value={name} />
         <Input
-            label="Epost"
+            label="Din epost"
+            placeholder="ola@nordmann.no"
             type="email"
             name="email"
             id="email"
             bind:value={email} />
+        <Input
+            label="Melding"
+            placeholder="Jeg trenger hjelp med..."
+            type="textarea"
+            name="message"
+            id="message"
+            bind:value={message} />
     </form>
 </div>
