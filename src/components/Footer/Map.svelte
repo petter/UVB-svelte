64f4fefb81326529f90342ee41e0830e8bd48f03
Petter Sæther Moen <pettersmoen@gmail.com>
2020-01-02 20:12:07 +0100

    Added simple marker with popup

--- src/components/Footer/Map.svelte
+++ src/components/Footer/Map.svelte
@@ -6,49 +6,71 @@
 
         mapboxgl.accessToken =
             'pk.eyJ1IjoicGV0dGVyc21vZW4iLCJhIjoiY2s0c3FvYmtlMDk5dzNscnNtNncyYjY2eiJ9.VZ7nRifBlRyCGBEUZ6F5bQ';
-        var map = new mapboxgl.Map({
+        const map = new mapboxgl.Map({
             container: 'map',
             style: 'mapbox://styles/mapbox/streets-v11',
             center: [8.677655, 62.987516],
             zoom: 11
         });
 
-        map.on('load', () => {
-            map.loadImage('https://i.imgur.com/MK4NUzI.png', function(
-                error,
-                image
-            ) {
-                if (error) throw error;
-                map.addImage('custom-marker', image);
-                /* Style layer: A style layer ties together the source and image and specifies how they are displayed on the map. */
-                map.addLayer({
-                    id: 'markers',
-                    type: 'symbol',
-                    /* Source: A data source specifies the geographic coordinate where the image marker gets placed. */
-                    source: {
-                        type: 'geojson',
-                        data: {
-                            type: 'FeatureCollection',
-                            features: [
-                                {
-                                    type: 'Feature',
-                                    properties: {},
-                                    geometry: {
-                                        type: 'Point',
-                                        coordinates: [8.677655, 62.98765]
-                                    }
-                                }
-                            ]
-                        }
+        const geojson = {
+            type: 'FeatureCollection',
+            features: [
+                {
+                    type: 'Feature',
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [8.677655, 62.987516]
                     },
-                    layout: {
-                        'icon-image': 'custom-marker'
+                    properties: {
+                        title: 'Surnadal Veterinærpraksis',
+                        description:
+                            'Ved folketshus i Surnadal ligger klinikken vår. Kom innom!'
                     }
-                });
-            });
+                }
+            ]
+        };
+
+        geojson.features.forEach(marker => {
+            const el = document.createElement('div');
+            el.className = 'marker';
+
+            new mapboxgl.Marker(el)
+                .setLngLat(marker.geometry.coordinates)
+                .setPopup(
+                    new mapboxgl.Popup({ offset: 25 }) // add popups
+                        .setHTML('<h3>' + marker.properties.title + '</h3>')
+                )
+                .addTo(map);
         });
     });
 </script>
+
+<style>
+    :global(.marker) {
+        background-image: url('/mapbox-icon.png');
+        background-size: cover;
+        width: 36px;
+        height: 36px;
+        border-radius: 50%;
+        cursor: pointer;
+    }
+
+    :global(.mapboxgl-popup) {
+        max-width: 200px;
+    }
+
+    :global(.mapboxgl-popup-content) {
+        text-align: center;
+        color: black;
+        font-family: 'Open Sans', sans-serif;
+    }
+
+    :global(.mapboxgl-popup-content h3) {
+        font-weight: bold;
+        font-size: 1.125rem;
+    }
+</style>
 
 <svelte:head>
     <link
