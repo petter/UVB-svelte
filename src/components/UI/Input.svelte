ec98dd3535f7ed5578c26c1c50adb9d8c491764f
Petter Sæther Moen <pettersmoen@gmail.com>
2020-01-02 20:54:23 +0100

    Added extra input field for phone, added required field to inputs

--- src/components/UI/Input.svelte
+++ src/components/UI/Input.svelte
@@ -9,6 +9,7 @@
     export let placeholder = '';
     export let id;
     export let value;
+    export let required = false;
 
     const classes = `p-2 text-gray-900 placeholder-gray-600 bg-gray-100 text-lg
         rounded border-2 border-gray-600 focus:bg-white focus:border-gray-900
@@ -28,7 +29,6 @@
         if (type !== 'textarea') {
             value = event.target.value;
         } else {
-            console.log(event);
             event.target.style.height = '0px';
             event.target.style.height = `${event.target.scrollHeight}px`;
         }
@@ -36,7 +36,12 @@
 </script>
 
 <div class="flex flex-col">
-    <label for={id} class="text-sm text-indigo-900">{label}</label>
+    <label for={id} class="text-sm text-indigo-900">
+        {label}
+        {#if required}
+            <span class="text-red-600 font-bold">*</span>
+        {/if}
+    </label>
     {#if type === 'textarea'}
         <textarea
             bind:this={textareaRef}
--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -4,6 +4,7 @@
 
     let name = '',
         email = '',
+        phone = '',
         message = '';
 </script>
 
@@ -13,6 +14,11 @@
 
 <div class="flex flex-col max-w-4xl w-full mx-auto p-6">
     <H1>For spørsmål eller timebestilling - kontakt oss her</H1>
+    <p>
+        Felt markert med
+        <span class="text-red-600 font-bold">*</span>
+        er påkrevd
+    </p>
     <form>
         <Input
             label="Ditt navn"
@@ -20,20 +26,30 @@
             type="text"
             name="name"
             id="name"
-            bind:value={name} />
+            bind:value={name}
+            required />
         <Input
             label="Din epost"
             placeholder="ola@nordmann.no"
             type="email"
             name="email"
             id="email"
-            bind:value={email} />
+            bind:value={email}
+            required />
+        <Input
+            label="Ditt telefonnummer"
+            placeholder="123 45 678"
+            type="tel"
+            name="phone"
+            id="phone"
+            bind:value={phone} />
         <Input
             label="Melding"
             placeholder="Jeg trenger hjelp med..."
             type="textarea"
             name="message"
             id="message"
-            bind:value={message} />
+            bind:value={message}
+            required />
     </form>
 </div>
