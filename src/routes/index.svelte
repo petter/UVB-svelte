6d3ae82cae7b96f58a01e89791795a097f9091e5
Petter Sæther Moen <pettersmoen@gmail.com>
2020-01-03 20:23:10 +0100

    Replaced header logo with better logo file and added bgimage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,15 +4,26 @@
     import { navHeight } from '../shared/constants';
 </script>
 
+<style>
+    header {
+        background-image: url(https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80);
+        background-size: cover;
+        background-position: center;
+    }
+</style>
+
 <svelte:head>
     <title>Surnadal veterinærpraksis</title>
 </svelte:head>
 
 <header
     id="header"
-    class="flex flex-grow -mt-{navHeight} h-screen w-full justify-center
-    items-center bg-gray-400">
-    <img src="logo.jpg" alt="Surnadal Veterinærpraksis logo" />
+    class="flex flex-grow flex-col -mt-{navHeight} h-screen w-full
+    justify-center md:justify-end items-center bg-black opacity-75 p-12">
+    <img
+        src="/img/logo/White.svg"
+        class="w-full md:w-1/2 opacity-75"
+        alt="Surnadal Veterinærpraksis logo" />
 </header>
 
 <About />
