81fa731a3db3cd39ef401dab9c48a85b0655ed86
Petter Sæther Moen <pettersmoen@gmail.com>
2020-01-03 20:08:11 +0100

    Changed map icon to logo

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -39,7 +39,13 @@
                 .setLngLat(marker.geometry.coordinates)
                 .setPopup(
                     new mapboxgl.Popup({ offset: 25 }) // add popups
-                        .setHTML('<h3>' + marker.properties.title + '</h3>')
+                        .setHTML(
+                            '<h3>' +
+                                marker.properties.title +
+                                '</h3><p>' +
+                                marker.properties.description +
+                                '</p>'
+                        )
                 )
                 .addTo(map);
         });
