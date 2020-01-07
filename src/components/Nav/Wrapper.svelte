0c70030ce54a47e2f5a675f036640fdbdb2455f8
Petter Sæther Moen <pettersmoen@gmail.com>
2020-01-07 14:20:48 +0100

    Fixed bug where changing page wouldn't affect nav bg

--- src/components/Nav/Wrapper.svelte
+++ src/components/Nav/Wrapper.svelte
@@ -10,7 +10,9 @@
     const scrollEventHandler = () => {
         if (isHome)
             navBg = window.scrollY < 50 ? 'bg-transparent' : 'bg-blue-900';
+        else navBg = 'bg-blue-900';
     };
+    $: isHome, scrollEventHandler();
 
     onMount(() => {
         scrollEventHandler();
@@ -29,7 +31,7 @@
     }
 </style>
 
-<div class="fixed z-30 top-0 left-0 w-full h-{navHeight} gradient" />
+<div class="fixed top-0 left-0 w-full h-{navHeight} gradient" />
 <nav
     class="fixed z-40 w-full h-{navHeight} flex items-center flex-wrap
     justify-between px-6 transition-bg transition-250 {navBg}
