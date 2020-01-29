df5e143c5168d50e33a40dcdb32edc0f039178cd
Petter Sæther Moen <pettersmoen@gmail.com>
2020-01-29 23:14:05 +0100

    Added a lot of pictures to carousel

--- src/components/Header/Carousel.svelte
+++ src/components/Header/Carousel.svelte
@@ -1,16 +1,16 @@
 <script>
-    export let images; // array of image urls
+    export let images; // array of {img : string, pos : string}
 
     let cur = 0;
 
     setInterval(() => {
         cur = (cur + 1) % images.length;
-    }, 10000);
+    }, 3000);
 </script>
 
-{#each images as image, i}
+{#each images as { img, pos }, i}
     <div
         class="absolute left-0 top-0 w-full h-full bg-cover bg-center
         transition-opacity transition-1000 {i === cur ? 'opacity-100' : 'opacity-0'}"
-        style="background-image: url({image})" />
+        style="background-image: url({img}); background-position: {pos}" />
 {/each}
