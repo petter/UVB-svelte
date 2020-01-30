b37a37da48376e97554991a0dc737584e6b9ba5e
Petter Sæther Moen <pettersmoen@gmail.com>
2020-01-30 22:27:02 +0100

    fixed visual bug with cards

--- src/components/About/Card.svelte
+++ src/components/About/Card.svelte
@@ -11,17 +11,20 @@
     export { className as class };
 </script>
 
-<div
-    class="flex flex-col lg:flex-row bg-indigo-200 items-center rounded-lg m-2
-    p-6 {className}">
-    <img
-        src={image}
-        alt="Bilde av {name}"
-        class="h-24 w-24 rounded-full lg:mr-4" />
-    <div class="text-center lg:text-left">
-        <h2 class="text-lg font-semibold">{name}</h2>
-        <div class="font-hairline text-indigo-800">{title}</div>
-        <div class="">{phone}</div>
-        <!-- <Link href="mailto:{email}">{email}</Link> -->
+<div class="p-2 w-full">
+    <div
+        class="flex flex-col lg:flex-row bg-indigo-200 items-center rounded-lg
+        p-6 {className}">
+        <img
+            src={image}
+            alt="Bilde av {name}"
+            class="h-24 w-24 rounded-full lg:mr-4" />
+        <div class="text-center lg:text-left">
+            <h2 class="text-lg font-semibold">{name}</h2>
+            <div class="font-hairline text-indigo-800">{title}</div>
+            <div class="">{phone}</div>
+            <!-- <Link href="mailto:{email}">{email}</Link> -->
+        </div>
     </div>
+
 </div>
