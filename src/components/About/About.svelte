fcf032470fee1d3ee4901e5e24e1c70609dd28e0
Petter Sæther Moen <pettersmoen@gmail.com>
2020-04-28 23:22:30 +0200

    Change cards on about page

--- src/components/About/About.svelte
+++ src/components/About/About.svelte
@@ -4,70 +4,71 @@
     import H1 from '../UI/H1.svelte';
     import Section from '../UI/Section.svelte';
 
-    // const cardClasses = 'w-full';
+    const team = [
+        {
+            name: 'Vigdis Sæther Moen Burger',
+            title: 'Daglig leder',
+            phone: '916 43 977',
+            images: [
+                { img: '/img/vigdispig.jpg', pos: '32% 13%' },
+                { img: '/img/vigdisandwit.jpg', pos: '64% 46%' },
+                { img: '/img/vigdishorse.jpg', pos: '43% 10%' }
+            ],
+            descriptionList: [
+                'Sisteårs veterinærstudent ved University of veterinary and pharmaceutical sciences Brno',
+                'Spesiell interesse for hest og produksjonsdyr, med fokus på skadeforebygging og reproduksjon ',
+                'Hestemenneske med mange år som aktiv konkurranse rytter'
+            ]
+        },
+        {
+            name: 'Uzi Burger DVM',
+            title: 'Veterinær',
+            phone: '929 88 307',
+            images: [
+                { img: '/img/uziandbaileywater.jpg', pos: '48% 44%' },
+                { img: '/img/uziandbaileywater.jpg', pos: '48% 44%' },
+                { img: '/img/uzidogsandcow.jpg', pos: '52% 62%' },
+                { img: '/img/uzikissingbelle.jpg', pos: '39% 37%' },
+                { img: '/img/uziinnature.jpg', pos: '46% 40%' },
+                { img: '/img/uzidogsandcow2.jpg', pos: '57% 38%' }
+            ],
+            descriptionList: [
+                'Veterinær fra University of veterinary and pharmaceutical sciences Brno',
+                'Spesiell interesse for hund, katt og eksotiske dyr',
+                '10 års erfaring i drift av veterinærklinikker i Israel',
+                'Lidenskapelig hundemenneske og fisker'
+            ]
+        }
+    ];
+
+    const samarbeidspartnere = [
+        {
+            name: 'Anders Sæther Moen DVM',
+            title: 'Veterinær',
+            phone: '905 29 219',
+            images: [{ img: '/img/anders.jpg', pos: '35% 25%' }],
+            descriptionList: [
+                'Veterinær fra Universitetet i Gent, Belgia',
+                'Produksjonsdyr og hesteveterinær',
+                'Dyrekiropraktor',
+                'Biomekanikk på hest'
+            ]
+        }
+    ];
+
+    const sections = [
+        { title: 'Vårt team', cards: team },
+        { title: 'Våre samarbeidspartnere', cards: samarbeidspartnere }
+    ];
 </script>
 
 <Section id="about">
-    <!-- <div class="flex flex-col">
-        <H1>Vårt team</H1>
-        <div
-            class="flex w-full flex-col lg:flex-row flex-1 justify-between py-6">
-            <Card
-                name="Vigdis Sæther Moen Burger"
-                title="Daglig leder"
-                phone="916 43 977"
-                image="/img/headshots/vigdis1.jpg"
-                description="Sisteårs veterinærstudent med fordypning i hest,
-                produksjonsdyr og eksotiske dyr."
-                descriptionList={['Sisteårs veterinærstudent ved University of veterinary and pharmaceutical sciences Brno', 'Spesiell interesse for hest og produksjonsdyr, med fokus på skadeforebygging og reproduksjon ', 'Hestemenneske med mange år som aktiv konkurranse rytter']}
-                class={cardClasses} />
-
-            <Card
-                name="Uzi Burger DVM"
-                title="Veterinær"
-                phone="929 88 307"
-                image="/img/headshots/uzi.jpg"
-                description="Veterinær med fordypning i eksotiske dyr og smådyr."
-                descriptionList={['Veterinær fra University of veterinary and pharmaceutical sciences Brno', 'Spesiell interesse for hund, katt og eksotiske dyr', '10 års erfaring i drift av veterinærklinikker i Israel', 'Lidenskapelig hundemenneske og fisker']}
-                class={cardClasses} />
-        </div>
-
-        <H1>Våre samarbeidspartnere</H1>
-
-        <div class="flex w-full flex-col flex-1 justify-between py-6">
-            <Card
-                name="Anders Sæther Moen DVM"
-                title="Veterinær"
-                phone="905 29 219"
-                image="/img/headshots/uzi.jpg"
-                descriptionList={['Veterinær fra Universitetet i Gent, Belgia', 'Produksjonsdyr og hesteveterinær', 'Dyrekiropraktor', 'Biomekanikk på hest']}
-                class={cardClasses} />
-        </div>
-    </div> -->
-    <div class="flex flex-col">
-        <H1>Vårt team</H1>
-        <NewCard
-            name="Vigdis Sæther Moen Burger"
-            title="Daglig leder"
-            phone="916 43 977"
-            image="/img/headshots/vigdis1.jpg"
-            description="Sisteårs veterinærstudent med fordypning i hest,
-            produksjonsdyr og eksotiske dyr."
-            descriptionList={['Sisteårs veterinærstudent ved University of veterinary and pharmaceutical sciences Brno', 'Spesiell interesse for hest og produksjonsdyr, med fokus på skadeforebygging og reproduksjon ', 'Hestemenneske med mange år som aktiv konkurranse rytter']} />
-        <NewCard
-            name="Uzi Burger DVM"
-            title="Veterinær"
-            phone="929 88 307"
-            image="/img/headshots/uzi.jpg"
-            description="Veterinær med fordypning i eksotiske dyr og smådyr."
-            descriptionList={['Veterinær fra University of veterinary and pharmaceutical sciences Brno', 'Spesiell interesse for hund, katt og eksotiske dyr', '10 års erfaring i drift av veterinærklinikker i Israel', 'Lidenskapelig hundemenneske og fisker']} />
-
-        <H1>Våre samarbeidspartnere</H1>
-        <NewCard
-            name="Anders Sæther Moen DVM"
-            title="Veterinær"
-            phone="905 29 219"
-            image="/img/headshots/uzi.jpg"
-            descriptionList={['Veterinær fra Universitetet i Gent, Belgia', 'Produksjonsdyr og hesteveterinær', 'Dyrekiropraktor', 'Biomekanikk på hest']} />
+    <div class="flex flex-col w-full max-w-4xl mx-auto">
+        {#each sections as { title, cards }}
+            <H1>{title}</H1>
+            {#each cards as card}
+                <NewCard {...card} />
+            {/each}
+        {/each}
     </div>
 </Section>
--- src/components/About/NewCard.svelte
+++ src/components/About/NewCard.svelte
@@ -1,5 +1,7 @@
 <script>
     import Carousel from '../UI/Carousel.svelte';
+    import Flex1 from '../UI/Containers/Flex1.svelte';
+    import FlexN from '../UI/Containers/FlexN.svelte';
 
     export let name;
     export let title;
@@ -10,11 +12,30 @@
     export let descriptionList;
 </script>
 
-<div class="w-full flex flex-row">
-    <Carousel {images} />
-    <div class="bg-indigo-300">
-        <div>{name}</div>
-        <div>{title}</div>
-        <div>{phone}</div>
-    </div>
+<style>
+    li {
+        list-style-type: '– ';
+    }
+</style>
+
+<div
+    class="w-full max-w-3xl m-2 flex flex-col sm:flex-row rounded-md
+    overflow-hidden">
+    <Flex1 class="relative min-h-1/4 sm:max-w-xs">
+        <Carousel {images} />
+    </Flex1>
+    <FlexN n="2" class="flex-col bg-indigo-200">
+        <div
+            class="w-full px-4 py-1 bg-indigo-300 text-blue-900
+            border-indigo-400">
+            <h2 class="text-xl font-bold">{name}</h2>
+            <div class="text-lg font-hairline text-indigo-800">{title}</div>
+            <div class="text-lg">{phone}</div>
+        </div>
+        <ul class="ml-2 px-4 py-2">
+            {#each descriptionList as desc}
+                <li class="my-2">{desc}</li>
+            {/each}
+        </ul>
+    </FlexN>
 </div>
