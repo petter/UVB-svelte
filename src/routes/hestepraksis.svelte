206956d37a697e71575f86cdd32eb7cd22dc20e9
Petter Sæther Moen <pettersmoen@gmail.com>
2020-04-29 22:37:26 +0200

    Add text to hestepraksis and produksjonsdyr pages

--- src/routes/hestepraksis.svelte
+++ src/routes/hestepraksis.svelte
@@ -1,5 +1,8 @@
 <script>
     import WithImageCarousel from '../components/Layouts/WithImageCarousel.svelte';
+    import H1 from '../components/UI/H1.svelte';
+    import Ul from '../components/UI/Ul.svelte';
+    import Section from '../components/UI/Section.svelte';
 
     const carouselImages = [
         { img: '/img/horse2.jpg', pos: '21% 43%' },
@@ -7,10 +10,30 @@
         { img: '/img/horse1.jpg', pos: '47% 46%' },
         { img: '/img/horseflehmenresponse.jpg', pos: '47% 46%' }
     ];
+
+    const lis = [
+        'Konsept/visjon: skadefri hest og prestasjon',
+        'Helsesjekk ved kjøp/salg/forsikring',
+        'Munnhuleundersøkelse og behandling',
+        'Halthetsutredelse',
+        'Enkelte ståede kirurgiske ingrep',
+        'Akkuttmedisin',
+        'Rehabilitering ',
+        'Treningsveiledning etter skade og forebyggende'
+    ];
 </script>
 
 <svelte:head>
     <title>Vårt team</title>
 </svelte:head>
 
-<WithImageCarousel images={carouselImages} />
+<WithImageCarousel images={carouselImages}>
+    <Section id="hestepraksis">
+        <H1>Konsept/visjon: skadefri hest og prestasjon</H1>
+        <Ul>
+            {#each lis as text}
+                <li>{text}</li>
+            {/each}
+        </Ul>
+    </Section>
+</WithImageCarousel>
--- src/routes/produksjonsdyr-og-veiledning.svelte
+++ src/routes/produksjonsdyr-og-veiledning.svelte
@@ -1,9 +1,18 @@
 <script>
     import WithImageCarousel from '../components/Layouts/WithImageCarousel.svelte';
+    import H1 from '../components/UI/H1.svelte';
+    import Ul from '../components/UI/Ul.svelte';
+    import Section from '../components/UI/Section.svelte';
 
     const carouselImages = [
         { img: '/img/cows.jpg', pos: '30% 44%' },
         { img: '/img/goats.jpg', pos: '0% 50%' }
+    ];
+
+    const lis = [
+        'Har som mål å ta del i produksjonsdyrpraksis og vaktgruppe i Surnadal fra høsten 2020.',
+        'Tilbyr besetningsveiledning med fokus på reproduksjon, kalvehelse og dyrevelferd',
+        'Har tatt kusignal kurs hos Tine veileder og er veldig fokusert på forebygende og produksjonsfremmende helsearbeid hvor miljø og drift er sentralt.'
     ];
 </script>
 
@@ -11,4 +20,13 @@
     <title>Vårt team</title>
 </svelte:head>
 
-<WithImageCarousel images={carouselImages} />
+<WithImageCarousel images={carouselImages}>
+    <Section id="produksjonsdyr-og-veiledning">
+        <H1>Produksjonsdyr og veiledning</H1>
+        <Ul>
+            {#each lis as text}
+                <li>{text}</li>
+            {/each}
+        </Ul>
+    </Section>
+</WithImageCarousel>
